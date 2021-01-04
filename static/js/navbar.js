b2f83910c1adc38f7eb98d9f87977ba26df2ac29
Alexander Peplowski <alexander.peplowski@gmail.com>
2021-01-03 22:13:27 -0500

    Removed navbar animations

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -7,29 +7,30 @@
     // ==================================================
     // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
     // $.onscroll = function() {scrollFunction()};
-    $(document).scroll(function () {
-      if ($(document).scrollTop() > 40) {
-        $('#top-navbar').removeClass('initial-navbar');
-        $('#top-navbar').addClass('final-navbar shadow');
+    // $(document).scroll(function () {
+    //   if ($(document).scrollTop() > 40) {
+    //     $('#top-navbar').removeClass('initial-navbar');
+    //     $('#top-navbar').addClass('final-navbar shadow');
 
-        $('#navbar-toggler').removeClass('navbar-dark');
-        $('#navbar-toggler').addClass('navbar-light');
+    //     $('#navbar-toggler').removeClass('navbar-dark');
+    //     $('#navbar-toggler').addClass('navbar-light');
 
-        // get the main logo from hidden img tag
-        let mainLogo = document.getElementById("main-logo").getAttribute("src");
-        $('#logo').attr("src", mainLogo);
-      } else {
-        $('#top-navbar').removeClass('final-navbar shadow');
-        $('#top-navbar').addClass('initial-navbar');
+    //     // get the main logo from hidden img tag
+    //     let mainLogo = document.getElementById("main-logo").getAttribute("src");
+    //     $('#logo').attr("src", mainLogo);
+    //   }
+    //   // } else {
+    //   //   $('#top-navbar').removeClass('final-navbar shadow');
+    //   //   $('#top-navbar').addClass('initial-navbar');
 
-        $('#navbar-toggler').removeClass('navbar-light');
-        $('#navbar-toggler').addClass('navbar-dark');
+    //   //   $('#navbar-toggler').removeClass('navbar-light');
+    //   //   $('#navbar-toggler').addClass('navbar-dark');
 
-        // get the inverted logo from hidden img tag
-        let invertedLogo = document.getElementById("inverted-logo").getAttribute("src");
-        $('#logo').attr("src", invertedLogo);
-      }
-    });
+    //   //   // get the inverted logo from hidden img tag
+    //   //   let invertedLogo = document.getElementById("inverted-logo").getAttribute("src");
+    //   //   $('#logo').attr("src", invertedLogo);
+    //   // }
+    // });
 
     // Creates a click handler to collapse the navigation when
     // anchors in the mobile nav pop up are clicked
